0e8f3ffea45a81da82fe8e58d5cb385502b9d6c8
lucas <lucasmastro93@gmail.com>
2022-12-01 12:56:01 -0300

    algo de responsive

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -11,15 +11,7 @@ const Checkout = () => {
   const [tel, setTel] = useState("")
   const [email, setEmail] = useState("")
   const [orderId, setOrderId] = useState("")
-  const [error, setError] = useState(false)
 
-  const expresiones = {
-    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    password: /^.{4,12}$/, // 4 a 12 digitos.
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-  }
 
   
   function handleClickBuyButton(e){
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,38 +1,109 @@
-import { AppBar, Button,  Toolbar, Typography} from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import CartWidget from './CartWidget';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 
+const drawerWidth = 240;
 
-const Navbar = () => {
-   
-    const pages= [
-        {label:"Productos",link:"/"},
-        {label:"Checkout", link:"/checkout"},
-        {label:"Contacto", link:"/contacto"},
-        
-    ]
-    return(
-<AppBar sx={{background:"#FFFFFF"}}>
-    <Toolbar>
-        <Link to="/"><img src="/assets/img/logo.jpg" alt="imagen logo" width={'80px'} height={'60px'}/></Link>
-        <Typography>SHOP</Typography>
-        
+const pages= [
+    {label:"Productos",link:"/"},
+    {label:"Checkout", link:"/checkout"},
+    {label:"Contacto", link:"/contacto"},
+    
+]
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}style={{background:'#8E1F4C'}} >
       
-        {pages.map((page) => (
-              <Button key={page.label} sx={{marginLeft:"10px"}} variant="contained" style={{background:'#8E1F4C'}}>
-                <Link to={page.link} style={{ color: '#FFF' }}>{page.label}</Link>
-              </Button  >
-            ))}
-        
+      <Divider />
+      <List >
+        {pages.map((item) => (
+          <ListItem key={item.label} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}><Link to={item.link} style={{ color: '#FFF' }}>{item.label}
+             </Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
-        <Button sx={{marginLeft:"auto"}} variant="contained" style={{background:'#8E1F4C'}}>Iniciar sesion</Button>
-        <Button sx={{marginLeft:"10px"}} variant="contained" style={{background:'#8E1F4C'}}>Registrarse</Button>
-        <Button sx={{marginLeft:"10px"}} variant="contained" style={{background:'#8E1F4C'}}><Link to="/cart" style={{ color: '#FFF' }}><CartWidget/></Link></Button>
-    </Toolbar>
-</AppBar>
-    );
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav" sx={{background:"#FFFFFF"}}>
+        <Toolbar>
+          <IconButton
+            color="primary"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Link to="/"><img src="/assets/img/logo.jpg" alt="imagen logo" width={'80px'} height={'60px'}/></Link>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {pages.map((item) => (
+              <Button key={item.label} sx={{marginLeft:"10px"}} variant="contained" style={{background:'#8E1F4C'}}  >
+                <Link to={item.link} style={{ color: '#FFF' }}>{item.label}</Link>
+                
+              </Button>
+            ))}
+          </Box>
+          <Button sx={{marginLeft:"auto"}} variant="contained" style={{background:'#8E1F4C'}}><Link to="/cart" style={{ color: '#FFF' }}><CartWidget/></Link></Button>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+        
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      
+    </Box>
+  );
+}
+
+Navbar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
 export default Navbar;
